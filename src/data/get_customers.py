b7c266e652c8eed1121b8f073282ccf8284ad08d
Ubuntu <ubuntu@ip-10-0-0-8.ec2.internal>
2019-09-21 16:54:46 +0000

    Initial commit

---
+++ src/data/get_customers.py
@@ -0,0 +1,28 @@
+import requests
+import json
+import os
+
+zfill_padding = 5
+maximum_number_of_requests = 1500
+api_key = os.getenv('TD_DAVINCI_API_KEY')
+destination_file_prefix = "customers/customers_"
+continuation_key = str()
+
+for i in range(maximum_number_of_requests):
+    print(f"Getting request #{i+1}...")
+
+    response = requests.post('https://api.td-davinci.com/api/raw-customer-data',
+        headers = { 'Authorization': api_key },
+        json={ 'continuationToken': continuation_key } )
+    response_data = response.json()
+    filename = destination_file_prefix + str(i).zfill(zfill_padding) + '.json'
+    continuation_key = response_data['result']['continuationToken']
+    
+    print(response)
+    print(f"Continuation key: {continuation_key}")
+
+    with(open(filename, 'w')) as w:
+        json.dump(response_data, w)
+    
+    if not continuation_key:
+        break
